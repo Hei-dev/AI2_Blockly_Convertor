3e97102b449692b1a2155cbac62b4ed1d2f7b30d
Hei-dev <anpromer71@gmail.com>
2021-11-27 21:21:23 +0800

    added comments

--- convert.js
+++ convert.js
@@ -3,7 +3,7 @@ function startConvert(bkyfile){
     var lstr
     var i;
     var bkstack;
-    for(i=0;i<bkyxml.length;i++){
+    for(i=0;i<bkyxml.length;i++){ //Special blocks that will be converted to differents block type.
         lstr = bkyxml[i]
         lstr = String(lstr).replace(/<block type="math_compare"/g,"<block type=\"logic_compare\"")
     }
